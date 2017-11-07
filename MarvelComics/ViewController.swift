4d7dbca1396b9cdabda824fe0e750b80743d4f90
Murat Sudan <tarumnadus@gmail.com>
2017-11-07 10:00:29 +0000

    Initial Commit

---
+++ MarvelComics/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MarvelComics
+//
+//  Created by Murat Sudan on 07/11/2017.
+//  Copyright © 2017 Tarum Nadus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
